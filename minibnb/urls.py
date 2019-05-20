6116cecd61ad8383416edbbea89d1bafbe264e66
David Kim <obadiah26@gmail.com>
2019-05-21 01:57:50 +0900

    urls.py 오타 수정

--- minibnb/urls.py
+++ minibnb/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path('user', include(user.urls)),
     path('property', include(property.urls)),
-    path('booking', incloude(booking.urls))
+    path('booking', include(booking.urls))
 ]
