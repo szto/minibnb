623d238bdbede21081d5ce3a414a7aad4433ba32
David Kim <obadiah26@gmail.com>
2019-05-22 12:19:57 +0900

    캐시 파일 제거 및 gitignore 설정

--- user/views.py
+++ user/views.py
@@ -1,5 +1,4 @@
-import jwt
-import json
+import requests, json, jwt
 
 from django.views import View
 from django.http import JsonResponse, HttpResponse
@@ -9,7 +8,21 @@ from .models import User
 
 class KakaoSignupView(View):
     def post(self, request):
-        payloads = json.loads(request.body)
+        #user_token = request.header['Authorization']
 
-        
-        
+        url = 'https://kapi.kakao.com/v2/user/me'
+        headers = {'Authorization':'KakaoAK d012af1a354d201b99dd7a9fac684141',
+                  'Content-type' : 'application/x-www-form-urlencoded;charset=utf-8',
+                }
+        data = {
+                'target_id_type':'user_id',
+                'target_id': '1088893870'
+                }
+        print(json.dumps(data))
+        kakao_response = requests.post(url, headers=headers , data=data , timeout=10)
+        print(kakao_response.status_code, kakao_response.request.headers, kakao_response.request.body)
+        print(kakao_response.text)
+
+        return HttpResponse(status=200)
+
+
