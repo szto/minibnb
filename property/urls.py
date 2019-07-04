bf6ef6dfc0dca3cd346a6dfd446bd11d5f3d9d7f
David Kim <obadiah26@gmail.com>
2019-07-05 03:00:22 +0900

    guest view, host view 작성 - 모델 수정

--- property/urls.py
+++ property/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import PropertyAllView, PropertyLookUpView, PropertyDetailView, PropertyCreateView, PropertyDeleteView
+from .views import (
+    PropertyAllView,
+    PropertyLookUpView,
+    PropertyDetailView,
+    PropertyCreateView,
+    PropertyDeleteView,
+)
 
 urlpatterns = [
     path('/all', PropertyAllView.as_view()),
     path('/', PropertyLookUpView.as_view()),
     path('/<int:pk>', PropertyDetailView.as_view()),
     path('/create' , PropertyCreateView.as_view()),
-    path('/delete', PropertyDeleteView.as_view())
+    path('/delete', PropertyDeleteView.as_view()),
+    path('/userid', PropertyDeleteView.as_view()),
+
 ]
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import KakaoSignupView
+from .views import (
+    KakaoSignupView,
+    UserLoginView,
+    GuestPageView,
+    HostPageView,
+    HostOrGuestView
+)
 
 urlpatterns = [
-    path('/kakao', KakaoSignupView.as_view()),        
+    path('/kakao', KakaoSignupView.as_view()),
+    path('/id', UserLoginView.as_view()),
+    path('/guestpage', GuestPageView.as_view()),
+    path('/hostpage', HostPageView.as_view()),
+    path('/ishost', HostOrGuestView.as_view()),
 ]
